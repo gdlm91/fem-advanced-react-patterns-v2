1b657cc022c82f25f9f9cef11da985dc36a730e0
Gerardo Leal <gerard.leal91@gmail.com>
2019-04-25 21:18:15 +1000

    Solution Exercise 3

--- src/exercises/03.js
+++ src/exercises/03.js
@@ -27,15 +27,28 @@ import {Switch} from '../switch'
 // ...
 
 // 🐨 create a ToggleContext with React.createContext here
+const ToggleContext = React.createContext()
 
 class Toggle extends React.Component {
   // 🐨 each of these compound components will need to be changed to use
   // ToggleContext.Consumer and rather than getting `on` and `toggle`
   // from props, it'll get it from the ToggleContext.Consumer value.
-  static On = ({on, children}) => (on ? children : null)
-  static Off = ({on, children}) => (on ? null : children)
-  static Button = ({on, toggle, ...props}) => (
-    <Switch on={on} onClick={toggle} {...props} />
+  static On = ({children}) => (
+    <ToggleContext.Consumer>
+      {({on}) => (on ? children : null)}
+    </ToggleContext.Consumer>
+  )
+  static Off = ({children}) => (
+    <ToggleContext.Consumer>
+      {({on}) => (!on ? children : null)}
+    </ToggleContext.Consumer>
+  )
+  static Button = props => (
+    <ToggleContext.Consumer>
+      {({on, toggle}) => (
+        <Switch on={on} onClick={toggle} {...props} />
+      )}
+    </ToggleContext.Consumer>
   )
   // Because we'll be passing state into context, we need to 🐨 add the
   // toggle function to state.
@@ -53,11 +66,12 @@ class Toggle extends React.Component {
     // this.props.children as the children of the provider. Then we'll
     // expose the on state and toggle method as properties in the context
     // value (the value prop).
-    return React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        on: this.state.on,
-        toggle: this.toggle,
-      }),
+    return (
+      <ToggleContext.Provider
+        value={{...this.state, toggle: this.toggle}}
+      >
+        {this.props.children}
+      </ToggleContext.Provider>
     )
   }
 }
